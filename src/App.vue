590f7efbd6577695943d9ee127fe17c7c03b66e5
leandro mortarini <leandromortarini2@gmail.com>
2025-05-09 15:43:47 -0300

    estructura

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <nav class="flex gap-3">
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/features">Features</RouterLink>
-    <RouterLink to="/pricing">Pricing</RouterLink>
-    <RouterLink to="/contact">Contact</RouterLink>
-  </nav>
-  <main class="bg-slate-200">
-    <RouterView />
-  </main>
-</template>
+  <div class="flex flex-col h-screen">
+    <!-- Header -->
+    <header class="flex items-center h-14 px-4 border-b border-gray-300 sm:h-16 md:px-6 lg:px-8">
+      <div>
+        <RouterLink class="flex items-center gap-2 font-semibold" to="#">
+          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
+        </RouterLink>
+      </div>
+      <nav class="ml-auto space-x-4 flex items-center h-10 sm:space-x-6">
+        <RouterLink to="/"> Home </RouterLink>
+        <RouterLink to="/features"> Features </RouterLink>
+        <RouterLink to="/pricing"> Pricing </RouterLink>
+        <RouterLink to="/contact"> Contact </RouterLink>
+      </nav>
+    </header>
+    <!-- Fin Header -->
 
-<script setup lang="ts">
-// import HomePage from './modules/landing/pages/HomePage.vue';
-</script>
+    <!-- Main -->
+    <main class="flex-1 flex items-center justify-center py-6">
+      <RouterView />
+    </main>
+    <!-- Fin Main -->
+
+    <!-- Footer -->
+    <footer class="flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8">
+      <p class="flex-1 text-sm text-gray-500 text-center">
+        © {{ new Date().getFullYear() }} Acme Corporation. Derechos reservados
+      </p>
+    </footer>
+    <!-- Fin Footer -->
+  </div>
+</template>
--- src/modules/landing/pages/HomePage.vue
+++ src/modules/landing/pages/HomePage.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1 class="text-3xl font-bold text-teal-500">Home</h1>
+  <div class="text-center">
+    <h1 class="text-4xl font-bold tracking-tighter sm:text-5xl">
+      Bienvenido RouterLink nuestro sitio web
+    </h1>
+    <p class="mx-auto max-w-[600px] text-gray-500 md:text-xl">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
+  </div>
+  >
 </template>
